12aa7a56317d1cdd254d7be404412fa0d3c705b6
Willmo103 <88165849+Willmo103@users.noreply.github.com>
2023-05-23 00:42:00 -0500

    more setting up docker

--- Dockerfile
+++ Dockerfile
@@ -6,6 +6,8 @@ COPY requirements.txt requirements.txt
 
 RUN pip install -r requirements.txt
 
+EXPOSE 4203
+
 COPY . .
 
 CMD ["python", "app.py"]
---
+++ run.py
@@ -0,0 +1,10 @@
+from app import app
+
+if __name__ == "__main__":
+        app.run(
+            host="127.0.0.1", port=5050, debug=True
+        ) # this is only for the development environment
+
+        # app.run(
+        #     host="127.0.0.1", port=
+        # ) # this is only for the development environment
